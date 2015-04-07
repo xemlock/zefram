a1a48c3561e89940f24efc3e1653dc2df527b722
xemlock <xemlock@gmail.com>
2015-04-07 21:54:32 +0200

    flash messenger update

--- library/Zefram/Controller/Action/Helper/FlashMessenger.php
+++ library/Zefram/Controller/Action/Helper/FlashMessenger.php
@@ -1,25 +1,52 @@
 <?php
 
-// According to http://stackoverflow.com/questions/1086075/zend-framework-flashmessenger-problem
-// there are PHP 5.2.x versions, that have a problem with 
-// Zend_Controller_Action_Helper_FlashMessenger::addMessage() (line 143):
-// self::$_session->{$this->_namespace}[] = $message;
-
-class Zefram_Controller_Action_Helper_FlashMessenger extends Zend_Controller_Action_Helper_FlashMessenger
+class Zefram_Controller_Action_Helper_FlashMessenger
+    extends Zend_Controller_Action_Helper_FlashMessenger
 {
+    /**
+     * Check if specific namespace(s) contains messages.
+     *
+     * @param  string|array $namespace OPTIONAL
+     * @return bool
+     */
+    public function hasMessages($namespace = null)
+    {
+        // handle null, otherwise the loop will not be entered
+        if ($namespace === null) {
+            $namespace = $this->getNamespace();
+        }
+
+        $namespaces = (array) $namespace;
+        foreach ($namespaces as $namespace) {
+            if (parent::hasMessages($namespace)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     public function addMessage($message, $namespace = null)
     {
         if (!is_string($namespace) || $namespace == '') {
             $namespace = $this->getNamespace();
         }
 
-        $count = isset(self::$_session->{$namespace}) 
-               ? count(self::$_session->{$namespace}) 
+        $count = isset(self::$_session->{$namespace})
+               ? count(self::$_session->{$namespace})
                : 0;
 
+        // According to http://stackoverflow.com/questions/1086075/zend-framework-flashmessenger-problem
+        // there are PHP 5.2.x versions, that have a problem with
+        // Zend_Controller_Action_Helper_FlashMessenger::addMessage() (line 143):
+        // self::$_session->{$this->_namespace}[] = $message;
+
         // line 143 issues the following notice:
-        //   Notice: Indirect modification of overloaded property 
-        //   Zend_Session_Namespace::$default has no effect in 
+        //   Notice: Indirect modification of overloaded property
+        //   Zend_Session_Namespace::$default has no effect in
         //   Zend\Controller\Action\Helper\FlashMessenger.php on line 143
         @parent::addMessage($message, $namespace);
 
@@ -30,5 +57,44 @@ class Zefram_Controller_Action_Helper_FlashMessenger extends Zend_Controller_Act
         }
 
         return $this;
+    }
+
+    /**
+     * Calls methods on flash messenger
+     *
+     * @param  string $method
+     * @param  array $args
+     * @throws BadMethodCallException
+     */
+    public function __call($method, $args)
+    {
+        // Recognize has<Namespace>Messages() and get<Namespace>Messages() methods
+        if (preg_match('/^(get|has)(\w+?)Messages$/i', $method, $matches)) {
+            $namespace = $this->_getCallNamespace($matches[2]);
+            $method = $matches[1] . 'Messages';
+            return call_user_func(array($this, $method), $namespace);
+        }
+
+        // Recognize add<Namespace>Message() method
+        if (preg_match('/^add(\w+?)Message$/i', $method, $matches)) {
+            $namespace = $this->_getCallNamespace($matches[1]);
+            return $this->addMessage(reset($args), $namespace);
+        }
+
+        throw new BadMethodCallException("Unrecognized method '$method()'");
+    }
+
+    /**
+     * Transforms namespace contained within method name passed to __call()
+     * to namespace actually stored in the session
+     *
+     * @param  string $namespace
+     * @return string
+     */
+    protected function _getCallNamespace($namespace)
+    {
+        // this effectively performs lcfirst() on a given namespace, but since
+        // the function is available since PHP 5.3.0 we cannot use it here
+        return strtolower(substr($namespace, 0, 1)) . substr($namespace, 1);
     }
 }
--- library/Zefram/View/Helper/FlashMessenger.php
+++ library/Zefram/View/Helper/FlashMessenger.php
@@ -2,9 +2,29 @@
 
 class Zefram_View_Helper_FlashMessenger extends Zend_View_Helper_Abstract
 {
+    /**
+     * @var Zend_Controller_Action_Helper_FlashMessenger
+     */
     protected $_flashMessenger;
 
-    protected function _getFlashMessenger()
+    /**
+     * Proxy method calls to flash messenger action helper
+     *
+     * @param  string $method
+     * @param  array  $args
+     * @return mixed
+     */
+    public function __call($method, $args)
+    {
+        return call_user_func_array(array($this->getFlashMessenger(), $method), $args);
+    }
+
+    /**
+     * Returns the flash messenger action helper
+     *
+     * @return Zend_Controller_Action_Helper_FlashMessenger
+     */
+    public function getFlashMessenger()
     {
         if (null === $this->_flashMessenger) {
             $this->_flashMessenger = Zend_Controller_Action_HelperBroker::getStaticHelper('flashMessenger');
@@ -14,28 +34,18 @@ class Zefram_View_Helper_FlashMessenger extends Zend_View_Helper_Abstract
 
     public function getMessage($namespace = null)
     {
-        foreach ($this->_getFlashMessenger()->getMessages($namespace) as $message) {
+        foreach ($this->getMessages($namespace) as $message) {
             return $message;
         }
-    }
-
-    public function getMessages($namespace = null)
-    {
-        return $this->_getFlashMessenger()->getMessages($namespace);
-    }
-
-    public function hasMessages($namespace = null)
-    {
-        return $this->_getFlashMessenger()->hasMessages($namespace);
-    }
-
-    public function flashMessenger()
-    {
-        return $this;
     }
 
     public function __toString()
     {
         return (string) $this->getMessage();
+    }
+
+    public function flashMessenger()
+    {
+        return $this;
     }
 }
