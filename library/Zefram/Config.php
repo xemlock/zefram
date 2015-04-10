3fec638f3941986f82ea4c7b3795a843e0047cfa
xemlock <xemlock@gmail.com>
2015-04-10 12:07:57 +0200

    updated config factory

--- library/Zefram/Config.php
+++ library/Zefram/Config.php
@@ -3,11 +3,11 @@
 class Zefram_Config extends Zend_Config
 {
     /**
-     * Creates a config object based on contents of a given file.
+     * Creates a config object based on the path of a given file.
      *
-     * @param string     $file      file to process
-     * @param string     $section   section to process
-     * @param array|bool $options
+     * @param  string     $file      file to process
+     * @param  string     $section   section to process
+     * @param  array|bool $options
      * @return Zend_Config
      * @throws Zend_Config_Exception When file cannot be loaded
      * @throws Zend_Config_Exception When section cannot be found in file contents
@@ -15,9 +15,11 @@ class Zefram_Config extends Zend_Config
     public static function factory($file, $section = null, $options = false)
     {
         $suffix = pathinfo($file, PATHINFO_EXTENSION);
-        $suffix = strtolower($suffix);
+        $suffix = ($suffix === 'dist')
+                ? pathinfo(basename($file, ".$suffix"), PATHINFO_EXTENSION)
+                : $suffix;
 
-        switch ($suffix) {
+        switch (strtolower($suffix)) {
             case 'ini':
                 $config = new Zefram_Config_Ini($file, $section, $options);
                 break;
@@ -35,12 +37,23 @@ class Zefram_Config extends Zend_Config
                 $config = new Zend_Config_Yaml($file, $section, $options);
                 break;
 
+            case 'php':
+            case 'inc':
+                $config = include $file;
+                if (!is_array($config)) {
+                    throw new Zend_Config_Exception(
+                        'Invalid configuration file provided; PHP file does not return array value'
+                    );
+                }
+                return $config;
+
             default:
-                throw new Zend_Config_Exception('Invalid configuration file provided; unknown config type: ' . $suffix);
+                throw new Zend_Config_Exception(sprintf(
+                    "Invalid configuration file provided; unknown config type '%s'",
+                    $suffix
+                ));
         }
 
         return $config;
     }
-
-
 }
