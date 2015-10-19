1c929daf8a069d914c6652713e725e1aeb4022c7
xemlock <xemlock@gmail.com>
2015-10-20 00:01:11 +0200

    Using bootstrap as container instead of directly using container

--- library/Zefram/Controller/Action/Helper/Resource.php
+++ library/Zefram/Controller/Action/Helper/Resource.php
@@ -11,79 +11,52 @@ class Zefram_Controller_Action_Helper_Resource
     extends Zend_Controller_Action_Helper_Abstract
 {
     /**
-     * @var Zend_Application_Bootstrap_BootstrapAbstract
-     */
-    protected $_container;
-
-    /**
-     * Set resource container
+     * Get bootstrap
      *
-     * @param object $container
+     * @return Zend_Application_Bootstrap_BootstrapAbstract
      * @throws Zend_Controller_Action_Exception
      */
-    public function setContainer($container)
+    public function getBootstrap()
     {
-        if (!is_object($container)) {
-            throw new Zend_Controller_Action_Exception('Resource container must be an object');
+        // All bootstrapped plugin resources have 'bootstrap' param.
+        // Zend_Application_Resource_Frontcontroller::init() sets the
+        // 'bootstrap' value as a Front Controller parameter. Hence the
+        // "official" way of accessing resource container, which is:
+        $bootstrap = $this->getFrontController()->getParam('bootstrap');
+        if ($bootstrap instanceof Zend_Application_Bootstrap_BootstrapAbstract) {
+            return $bootstrap;
         }
-        $this->_container = $container;
-        return $this;
+        throw new Zend_Controller_Action_Exception('Unable to retrieve application bootstrap');
     }
 
     /**
-     * Get resource container
-     *
-     * @return object
-     */
-    public function getContainer()
-    {
-        if (empty($this->_container)) {
-            // All bootstrapped plugin resources have 'bootstrap' param.
-            // Zend_Application_Resource_Frontcontroller::init() sets the
-            // 'bootstrap' value as a Front Controller param. Hence the
-            // "official" way of accessing resource container.
-            $bootstrap = $this->getFrontController()->getParam('bootstrap');
-            if ($bootstrap instanceof Zend_Application_Bootstrap_BootstrapAbstract) {
-                $container = $bootstrap->getContainer();
-            }
-            // if no container was retrieved from bootstrap (or there was no
-            // bootstrap at all) fall back to Zend_Registry instance.
-            if (empty($container)) {
-                $container = Zend_Registry::getInstance();
-            }
-            $this->setContainer($container);
-        }
-        return $this->_container;
-    }
-
-    /**
-     * Retrieve resource from container
+     * Retrieve a resource from bootstrap
      *
      * @param  string $resource
+     * @param  bool $throw OPTIONAL
      * @return mixed
      * @throws Zend_Controller_Action_Exception
      */
-    public function getResource($resource)
+    public function getResource($resource, $throw = true)
     {
         $resource = (string) $resource;
-        $container = $this->getContainer();
+        $bootstrap = $this->getBootstrap();
 
-        // Check if container is compatible with container-interop API
-        // (https://github.com/container-interop/container-interop)
-        if (method_exists($container, 'get')) {
-            return $container->get($resource);
-        } elseif (isset($container->{$resource})) {
-            return $container->{$resource};
+        if ($bootstrap->hasResource($resource)) {
+            return $bootstrap->getResource($resource);
         }
 
-        throw new Zend_Controller_Action_Exception(sprintf(
-            'Resource matching "%s" not found',
-            $resource
-        ));
+        if ($throw) {
+            throw new Zend_Controller_Action_Exception(sprintf(
+                'Resource matching "%s" not found',
+                $resource
+            ));
+        }
+        return null;
     }
 
     /**
-     * Proxy to {@link getResource()}.
+     * Proxy to {@link getResource()}
      *
      * @param  string $resource
      * @return mixed
