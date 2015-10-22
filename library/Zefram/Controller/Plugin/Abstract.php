9a5583c746b85b3828ca2cfc3c9cab75b248f3f5
xemlock <xemlock@gmail.com>
2015-10-22 22:29:00 +0200

    initialization of resource helper if needed

--- library/Zefram/Controller/Plugin/Abstract.php
+++ library/Zefram/Controller/Plugin/Abstract.php
@@ -12,7 +12,12 @@ abstract class Zefram_Controller_Plugin_Abstract extends Zend_Controller_Plugin_
     public function getResource($resource, $throw = true)
     {
         /** @var $helper Zefram_Controller_Action_Helper_Resource */
-        $helper = Zend_Controller_Action_HelperBroker::getStaticHelper('Resource');
+        try {
+            $helper = Zend_Controller_Action_HelperBroker::getStaticHelper('Resource');
+        } catch (Zend_Controller_Action_Exception $exception) {
+            $helper = new Zefram_Controller_Action_Helper_Resource();
+            Zend_Controller_Action_HelperBroker::addHelper($helper);
+        }
         return $helper->getResource($resource, $throw);
     }
 }
