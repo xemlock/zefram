bf68b5f28851270f98056b9f8cd2cc70ba7d8ab1
xemlock <xemlock@gmail.com>
2015-12-16 08:42:37 +0100

    added module no controller path spec

--- library/Zefram/Controller/Action/Helper/ViewRenderer.php
+++ library/Zefram/Controller/Action/Helper/ViewRenderer.php
@@ -3,6 +3,8 @@
 /**
  * A enhanced replacement for {@link Zend_Controller_Action_Helper_ViewRenderer}
  *
+ * This helper allows to specify path specifications for each module separately.
+ *
  * @author xemlock <xemlock@gmail.com>
  */
 class Zefram_Controller_Action_Helper_ViewRenderer
@@ -12,6 +14,11 @@ class Zefram_Controller_Action_Helper_ViewRenderer
      * @var array
      */
     protected $_moduleViewScriptPathSpec;
+
+    /**
+     * @var array
+     */
+    protected $_moduleViewScriptPathNoControllerSpec;
 
     public function getViewScriptPathSpec($module = null)
     {
@@ -30,6 +37,27 @@ class Zefram_Controller_Action_Helper_ViewRenderer
             $this->_viewScriptPathSpec = (string) $path;
         } else {
             $this->_moduleViewScriptPathSpec[$module] = (string) $path;
+        }
+        return $this;
+    }
+
+    public function getViewScriptPathNoControllerSpec($module = null)
+    {
+        if (null === $module) {
+            $module = $this->getModule();
+        }
+        if (isset($this->_moduleViewScriptPathNoControllerSpec[$module])) {
+            return $this->_moduleViewScriptPathNoControllerSpec[$module];
+        }
+        return $this->_moduleViewScriptPathNoControllerSpec;
+    }
+
+    public function setViewScriptPathNoControllerSpec($path, $module = null)
+    {
+        if (null === $module) {
+            $this->_moduleViewScriptPathNoControllerSpec = (string) $path;
+        } else {
+            $this->_moduleViewScriptPathNoControllerSpec[$module] = (string) $path;
         }
         return $this;
     }
