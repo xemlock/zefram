232cdc4e62f7d2e57a39a027e702f5a43d406c7e
xemlock <xemlock@gmail.com>
2016-05-08 13:31:46 +0200

    Slug filter does not extends Translit, fixed filterStatic

--- library/Zefram/Filter/Slug.php
+++ library/Zefram/Filter/Slug.php
@@ -6,19 +6,93 @@
  * @version 2014-07-31
  * @author  xemlock
  */ 
-class Zefram_Filter_Slug extends Zefram_Filter_Translit
+class Zefram_Filter_Slug implements Zend_Filter_Interface
 {
-    public function filter($value)
-    {
-        $value = parent::filter($value);
+    /**
+     * Transliterator used for slug generation
+     * @var Zefram_Filter_Translit
+     */
+    protected $_transliterator;
 
-        $value = trim($value, " -_\r\n\v\f");
-        $value = preg_replace(array(
+    /**
+     * Returns the transliterator used for slug generation.
+     *
+     * @return Zefram_Filter_Translit
+     */
+    public function getTransliterator()
+    {
+        return $this->_transliterator;
+    }
+
+    /**
+     * Sets the transliterator used for slug generation.
+     *
+     * @param Zefram_Filter_Translit $transliterator
+     * @return Zefram_Filter_Slug
+     */
+    public function setTransliterator(Zefram_Filter_Translit $transliterator = null)
+    {
+        $this->_transliterator = $transliterator;
+        return $this;
+    }
+
+    /**
+     * Generate slug from given string.
+     *
+     * @param string $string Input string to be filtered
+     * @return string A generated slug
+     */
+    public function filter($string)
+    {
+        return self::filterStatic($string, array('transliterator' => $this->getTransliterator()));
+    }
+
+    /**
+     * @var Zefram_Filter_Translit
+     */
+    protected static $_defaultTransliterator;
+
+    /**
+     * Generate slug from given string without requiring separate instantiation of the filter object.
+     *
+     * @param string $string Input string to be filtered
+     * @param array $options Array of options controlling filter behaviour
+     * @return string A generated slug
+     * @throws Zend_Filter_Exception When provided transliterator is not an instance of Zend_Filter_Interface
+     */
+    public static function filterStatic($string, array $options = null)
+    {
+        if (isset($options['transliterator'])) {
+            $transliterator = $options['transliterator'];
+            if (!$transliterator instanceof Zend_Filter_Interface) {
+                throw new Zend_Filter_Exception('Transliterator must be an instance of Zend_Filter_Interface');
+            }
+        } else {
+            $transliterator = self::getDefaultTransliterator();
+        }
+
+        $string = $transliterator->filter($string);
+
+        $string = preg_replace(array(
             '/[^-0-9a-z]/i',
             '/-+/',
-        ), '-', $value);
-        $value = strtolower($value);
+        ), '-', $string);
+        $string = trim($string, '-');
+        $string = strtolower($string);
 
-        return $value;
+        return $string;
+    }
+
+    /**
+     * Returns the default transliterator.
+     *
+     * @return Zefram_Filter_Translit
+     */
+    public static function getDefaultTransliterator()
+    {
+        if (self::$_defaultTransliterator === null) {
+            self::$_defaultTransliterator = new Zefram_Filter_Translit();
+        }
+        return self::$_defaultTransliterator;
     }
 }
