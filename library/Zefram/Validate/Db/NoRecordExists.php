8003cb8b956bea83ae49300ead427a47fd2a7fb3
xemlock <xemlock@gmail.com>
2016-05-15 20:50:37 +0200

    Db validators accept DbTable instance as a table

--- library/Zefram/Validate/Db/NoRecordExists.php
+++ library/Zefram/Validate/Db/NoRecordExists.php
@@ -3,6 +3,20 @@
 class Zefram_Validate_Db_NoRecordExists extends Zend_Validate_Db_NoRecordExists
 {
     /**
+     * @param string|Zend_Db_Table_Abstract $table
+     * @return Zend_Validate_Db_Abstract
+     */
+    public function setTable($table)
+    {
+        if ($table instanceof Zend_Db_Table_Abstract) {
+            $this->setAdapter($table->getAdapter());
+            $this->setSchema($table->info(Zend_Db_Table_Abstract::SCHEMA));
+            $table = $table->info(Zend_Db_Table_Abstract::NAME);
+        }
+        return parent::setTable($table);
+    }
+
+    /**
      * @param  string|Zend_Db_Expr $field
      * @return Zefram_Validate_Db_RecordExists
      */
