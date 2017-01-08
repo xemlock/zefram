ac904c6c6cf8934633b0030948a9b2e2f81a5fbd
xemlock <xemlock@gmail.com>
2017-01-09 00:05:58 +0100

    Split HiddenArray logic

---
+++ library/Zefram/Form/Element/Array.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * @version 2014-04-30
+ */
+abstract class Zefram_Form_Element_Array extends Zend_Form_Element
+{
+    /**
+     * @var bool
+     */
+    protected $_isArray = true;
+
+    /**
+     * Does nothing as this element always represents an array.
+     *
+     * @param  bool $flag
+     * @return Zefram_Form_Element_HiddenArray
+     */
+    public function setIsArray($flag)
+    {
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getValue()
+    {
+        $valueFiltered = (array) $this->_value;
+        array_walk_recursive($valueFiltered, array($this, '_filterValue'));
+        return $valueFiltered;
+    }
+
+    /**
+     * @return array
+     */
+    public function getUnfilteredValue()
+    {
+        return (array) $this->_value;
+    }
+
+    /**
+     * @param  array $value
+     * @return Zefram_Form_Element_HiddenArray
+     */
+    public function setValue($value)
+    {
+        $this->_value = (array) $value;
+        return $this;
+    }
+
+    /**
+     * @param  mixed $value
+     * @param  array $context OPTIONAL
+     * @return bool
+     */
+    public function isValid($value, $context = null)
+    {
+        // isArray must be false when validating, and true when rendering
+        $this->_isArray = false;
+        $valid = parent::isValid($value, $context);
+        $this->_isArray = true;
+        return $valid;
+    }
+
+    /**
+     * Although this element represents an array, all array values are
+     * treated as a single value. Therefore error messages apply to all
+     * array of values, not to a single value.
+     *
+     * @return array
+     */
+    protected function _getErrorMessages()
+    {
+        $translator = $this->getTranslator();
+        $messages = $this->getErrorMessages();
+        $value = null;
+
+        foreach ($messages as $key => $message) {
+            if (null !== $translator) {
+                $message = $translator->translate($message);
+            }
+            if (false !== strpos($message, '%value%')) {
+                if (null === $value) {
+                    $value = implode(', ', $this->getValue());
+                }
+                $message = str_replace('%value%', $value, $message);
+            }
+            $messages[$key] = $message;
+        }
+        return $messages;
+    }
+}
