9d43ee178cb77fe16568d8365c032737f1aa27c2
xemlock <xemlock@gmail.com>
2017-06-09 09:20:44 +0200

    Use original impl of setBootstrap()

--- library/Zefram/Application.php
+++ library/Zefram/Application.php
@@ -3,28 +3,11 @@
 class Zefram_Application extends Zend_Application
 {
     /**
-     * Set bootstrap path/class
+     * Get bootstrap object
      *
-     * @param string $path  If the $class parameter is empty and $path is a
-     *                      valid class name, it will be used as the bootstrap
-     *                      class
-     * @param string $class If given it will be used as the bootstrap class
-     *                      name, otherwise a default 'Bootstrap' class name
-     *                      will be used
-     * @return Zefram_Application
-     */
-    public function setBootstrap($path, $class = null)
-    {
-        if (null === $class) {
-            if (class_exists($path, false)) {
-                $class = $path;
-                $path = null;
-            }
-        }
-        return parent::setBootstrap($path, $class);
-    }
-
-    /**
+     * If no bootstrap is present it will be initialized with an instance of
+     * {@link Zefram_Application_Bootstrap_Bootstrap}.
+     *
      * @return Zend_Application_Bootstrap_BootstrapAbstract
      */
     public function getBootstrap()
