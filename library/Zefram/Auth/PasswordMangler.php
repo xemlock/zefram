6ba40718fea8766b6b044355079523bcb0f0f854
xemlock <xemlock@gmail.com>
2019-03-10 00:45:35 +0100

    Remove obsolete/unused classes

--- library/Zefram/Auth/PasswordMangler.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-abstract class Zefram_Auth_PasswordMangler
-{
-    abstract public function mangle($password);
-    abstract public function validate($password, $challenge, $context = null);
-}
