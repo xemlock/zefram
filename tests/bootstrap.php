d110a5ecb6763f9df6ab6cd1496d6ca52fe60381
xemlock <952555+xemlock@users.noreply.github.com>
2023-12-10 13:36:41 +0100

    tests/bootstrap.php: Print correct ZF version + package name

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -23,7 +23,37 @@ if (!class_exists('PHPUnit_Framework_AssertionFailedError') && class_exists('PHP
     class_alias('PHPUnit\Framework\AssertionFailedError', 'PHPUnit_Framework_AssertionFailedError');
 }
 
-echo "Zend Framework version: ", Zend_Version::VERSION, "\n";
+function getZendFrameworkVersion() {
+    // In some community maintained distributions Zend_Version class may not exist,
+    // Zend_Config class however is widely used by the ZF internals.
+    // Additionally version in Zend_Version::VERSION can be out of sync from the
+    // version in composer.json.
+    $anchorClass = new ReflectionClass('Zend_Config');
+    $packageName = null;
+    $packageVersion = class_exists('Zend_Version') ? Zend_Version::VERSION : null;
+
+    $dir = dirname($anchorClass->getFileName());
+    while (true) {
+        if (file_exists($composerJson = $dir . '/composer.json')) {
+            $package = json_decode(file_get_contents($composerJson));
+            $version = \Composer\InstalledVersions::getPrettyVersion($package->name);
+            if ($version) {
+                $packageName = $package->name;
+                $packageVersion = $version;
+                break;
+            }
+        }
+        if (($parent = realpath($dir . '/..')) === $dir) {
+            break;
+        }
+        $dir = $parent;
+    }
+
+    return $packageVersion
+        . ($packageName && $packageName !== 'zendframework/zendframework1' ? " ({$packageName})" : '');
+};
+
+echo "Zend Framework version: ", getZendFrameworkVersion(), "\n";
 echo "PHP version:            ", PHP_VERSION, "\n";
 echo "PHP memory limit:       ", ini_get('memory_limit'), "\n";
 echo "\n";
